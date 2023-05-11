cb320c1cf0e41528aef32ab089bb64b3453ad317
stjos <133094710+stjos@users.noreply.github.com>
2023-05-11 11:02:33 +0800

    begin to sign

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AllTheHere
+//
+//  Created by Hong on 4/8/13.
+//  Copyright (c) 2013 CFCA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
